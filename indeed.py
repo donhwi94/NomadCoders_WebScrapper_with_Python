e5a4bfd029016df6a4b1a08bc459b25c334c8bdc
Replit user <replituser@example.com>
2021-03-25 17:58:37 +0000

    test2

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 10
 URL = f"https://kr.indeed.com/jobs?q=python&l=%EB%8C%80%EA%B5%AC&limit={LIMIT}"
 
-# 마지막 페이지를 추출하는 함수 
+# 마지막 페이지를 추출하는 함수이다 
 def extract_indeed_pages():
   result = requests.get(URL)
   soup = BeautifulSoup(result.text, "html.parser")
